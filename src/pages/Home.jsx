e0ac7c1acc2faafeaea43e734e89d223729410e4
vmrenter-code <vmrenter@uci.edu>
2026-02-28 13:13:10 -0800

    Floating hands

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import HeroHeader from "../components/home/HeroHeader";
+import FloatingHandSign from "../components/home/FloatingHandSign";
 
 const Home = () => {
   return (
     <div className="min-h-screen bg-white">
       <HeroHeader />
+      <FloatingHandSign /> 
     </div>
   );
 };
