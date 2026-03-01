6558fe53bbfd84f6950de1918c02273a73065e36
vmrenter-code <vmrenter@uci.edu>
2026-02-28 22:23:18 -0800

    Word game mode

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GestureDemo from "./pages/GestureDemo";
 import Blitz from "./components/games/Blitz.jsx";
 import Practice from "./components/games/Practice.jsx";
+import Words from "./components/games/Words.jsx";
 import Home from "./pages/Home.jsx";
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/demo" element={<GestureDemo />} />
         <Route path="/games/blitz" element={<Blitz />} />
         <Route path="/games/practice" element={<Practice />} />
+        <Route path="/games/words" element ={<Words />} />
       </Routes>
     </Router>
   );
